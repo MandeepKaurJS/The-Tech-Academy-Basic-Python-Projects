7bb654455f5b8724c48bf7504fa70cac3329aa7f
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-08 16:32:51 -0700

    Update Testprogram

--- TestProgram.py
+++ TestProgram.py
@@ -3,6 +3,6 @@ mySentence='I love the color'
  
 color_list=['Red','Blue','Green','Pink','Teal','Black']
 def Color_function():
-     print(color_list[0])
+     print("".format(color_list[0]))
 
 Color_function()
