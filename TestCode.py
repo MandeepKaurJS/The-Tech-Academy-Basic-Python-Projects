933f707415b8d1b79818ad4d3fd937676c726c8a
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-11 10:30:37 -0700

    Created 2 files

---
+++ TestCode.py
@@ -0,0 +1,10 @@
+#This is mt note
+"""for longer comment
+here is another line for testing
+you can write more line
+than """
+def printMe():
+    """this is my docstring
+    """
+    print("Me")
+print(printMe.__doc__)
---
+++ importmodule.py
@@ -0,0 +1,6 @@
+import ImportExternalmethods
+
+if __name__ == "__main__":
+    results=ImportExternalmethods.getNumber(5,9)
+    print(results)
+    
