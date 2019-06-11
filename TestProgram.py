77b521452d1a77c71408d6781b3ce1e8dc25c068
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-11 12:59:52 -0700

    Update TestProgram with methods and dunder methods

--- TestProgram.py
+++ TestProgram.py
@@ -12,3 +12,26 @@ def Color_function(name):
 lst= Color_function("Mandeep")
 for i in lst:
     print(i)
+def printName():
+    print("I absolutely \nlove coding \nwith Python!".format())
+def getName(name = ""):
+    name = askName(name)
+    print("Thank you, welcome {}!".format(name))
+
+
+def askName(name):
+    go = True
+    while go:
+        if name == "":
+            name = input("Please enter your name:\n>>> ")
+            if name != "":
+                go = False
+                
+    return name
+
+
+if __name__ == '__main__':
+    getName()
+
+"""if __name__ == '__main__':
+    printName()"""
