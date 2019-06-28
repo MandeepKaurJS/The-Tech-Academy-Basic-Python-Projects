5752bf1d4a5e0bda0f1809bdbcc449a93121b7ee
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-27 22:15:28 -0700

    update

--- python with json/Bs_4_python.py
+++ python with json/Bs_4_python.py
@@ -1,21 +1,22 @@
+from django.shortcuts import render
+
+import requests
+import re
 from bs4 import BeautifulSoup
 import requests
+import re
 
-#Using the requests module ,we use the "get" function
-#proivided to access the webpage provided as an
-#argument to this function
-result=requests.get("https://www.google.com/")
+# Download IMDB's Top 250 data
+url = 'http://www.imdb.com/chart/top'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+#Get all necessary information from the IMDb webpage (title, link, cast)
+movies = soup.select('td.titleColumn')+soup.select('td.posterColumn')
 
-#to make sure that  the website is accessible, we can
-#ensure that we obtain a 200 ok response to indicate
-#that the page is indeed present:
-#print(result.status_code)
-'''for other potential status codes you may encounter
-consult the following wikipedia page:
-https://en.wikipedia.org/wiki/List_of_HTTP_status_Codes
-We can also check the HTTP header od the website to
-verufy that we have indeed accessed the correct page:
-print(result.headers)
-for more information on http header and the information
-on can otain from them, you may consult :
-https://en.wikipedia.org/wiki/List_of_HTTP_header_fields'''
+
+#creating list to store movie titles
+
+# Store each item into dictionary (data), then put those into a list (imdb)
+for index in movies:
+		print(index)
+
